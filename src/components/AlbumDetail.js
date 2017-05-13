0fa9dbab79ecd34d347e84a7e52253e18657f893
Luqman <luqmanrom@gmail.com>
2017-05-13 19:38:40 +0800

    Add albumDetail and Card

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const AlbumDetail = (props) => {
+
+	return (
+		<View>
+			<Text> {props.album.title}</Text>
+		</View>
+	);
+
+};
+
+
+export default AlbumDetail;
---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View} from 'react-native';
+
+
+const Card = () => {
+
+	return (
+	
+		<View> </View>
+	);
+
+
+};
+
+
+export default Card;
