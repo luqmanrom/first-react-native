a511efcdb1a00333522bdab148051fa9530d85e1
Luqman <luqmanrom@gmail.com>
2017-05-13 20:20:31 +0800

    Add albumdetail and image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -11,12 +11,35 @@ const AlbumDetail = (props) => {
 	return (
 		<Card>
 			<CardSection>
-				<Text> {props.album.title}</Text>
+
+				<View>
+
+					<Image 
+					source={{ uri : props.album.thumbnail_image }} 
+					style={styles.thumbnailStyle} // If no width and image set, it will be zero hence not visible
+					/>
+				</View>
+
+				<View style={styles.headerContentStyle}>
+					<Text> {props.album.title}</Text>
+					<Text> {props.album.artist}</Text>
+				</View>
 			</CardSection>
-			
+
 		</Card>
 	);
 
 };
+
+const styles = {
+	headerContentStyle : {
+		flexDirection : 'column',
+		justifyContent: 'space-around'
+	},
+	thumbnailStyle : {
+		height: 50,
+		width: 50
+	}
+}
 
 export default AlbumDetail;
