d8d9eafb3745b63828bdb987d9874dcfea171d07
Luqman <luqmanrom@gmail.com>
2017-05-13 14:15:52 +0800

    Component nesting

---
+++ index.ios.js
@@ -0,0 +1,27 @@
+
+
+// Import a lib to create a component
+
+import React from 'react';
+
+// Import destructuring
+import {
+  AppRegistry,
+  Text
+} from 'react-native'; 
+
+import Header from './src/components/header';
+
+// Create component
+// Component is a JS function that returns some amount JSX code
+
+const App = () => {
+
+  return (
+    <Header />
+  );
+};
+
+// Render it, to show on screen
+
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+
+// Import libraries
+
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+
+const Header = () => {
+
+	return (
+		<Text> Albums!</Text>
+	);
+
+};
+
+// Export component to make it available on other part of the apps
+
+export default Header;
